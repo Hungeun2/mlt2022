031588748423152ec4a9a874f624c2f288d98fe7
Hungeun2 <bepoohun@gmail.com>
2022-09-27 09:25:53 +0900

    Feat: Deatail Page Styling

---
+++ src/style/DetailStyle.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Head = styled.div`
+  width: 100%;
+  max-width: 500px;
+  height: 70px;
+  font-size: 20px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+export const Table = styled.table`
+  border: 1px solid red;
+  width: 100%;
+  max-width: 500px;
+  border-collapse: collapse;
+  th,
+  td {
+    text-align: center;
+    border: 1px solid black;
+  }
+  .otherDay {
+    background-color: lightgray;
+  }
+`;
