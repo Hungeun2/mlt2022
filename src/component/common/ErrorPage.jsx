1b451f5993cb5b6cac826f2106c2fda05ebe842c
Hungeun2 <bepoohun@gmail.com>
2022-10-13 10:43:18 +0900

    Feat: Add Error Page & Style

---
+++ src/component/common/ErrorPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { ErrorStyle } from 'style/ErrorStyle';
+
+const ErrorPage = () => {
+  return <ErrorStyle>Error!</ErrorStyle>;
+};
+
+export default ErrorPage;
---
+++ src/style/ErrorStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const ErrorStyle = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 30px auto;
+  max-width: 400px;
+  height: 400px;
+  font-size: 30px;
+  font-weight: 700;
+`;
