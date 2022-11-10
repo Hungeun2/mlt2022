aa1cd7f91dfbb1ba3cb1a738e79bbee04136f759
Hungeun2 <bepoohun@gmail.com>
2022-11-10 14:09:08 +0900

    Style: Add Addbutton style

---
+++ src/style/AddButtonStyle.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const AddButtonStyle = styled.div`
+  margin: 10% auto;
+  width: 50%;
+  button {
+    width: 100%;
+    height: 45px;
+    border: 1px solid #d0d7de;
+    border-radius: 6px;
+    background-color: white;
+    color: #0969da;
+    font-size: 15px;
+    font-weight: 700;
+  }
+`;
